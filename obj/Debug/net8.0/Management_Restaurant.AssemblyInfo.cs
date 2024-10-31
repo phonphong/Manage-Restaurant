6a083a211ac6a0209d0f9b998b96c482c80ea228
nguyendinh <nguyendinhphong1518@gmail.com>
2024-10-31 20:04:39 +0700

    Saving local changes before pulling

--- obj/Debug/net8.0/Management_Restaurant.AssemblyInfo.cs
+++ obj/Debug/net8.0/Management_Restaurant.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Management_Restaurant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2eadb4e9ee50c7ff34869ac1dab66e4f1b42e4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Management_Restaurant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Management_Restaurant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
